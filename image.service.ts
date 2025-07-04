fec0586309b6505fddb5c8bf67b80fce5dcf4973
NehaShree34 <shreeneha555@gmail.com>
2025-07-04 22:23:05 +0530

    Add files via upload

---
+++ image.service.ts
@@ -0,0 +1,34 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({ providedIn: 'root' })
+export class ImageService {
+  private baseUrl = 'http://localhost:8080/api/images';
+
+  constructor(private http: HttpClient) {}
+
+  uploadImage(file: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.http.post(this.baseUrl + '/upload', formData);
+  }
+
+  getImages(): Observable<any> {
+    return this.http.get(this.baseUrl);
+  }
+
+  getImageUrl(id: number): string {
+    return `${this.baseUrl}/${id}`;
+  }
+
+  deleteImage(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
+}
+function Injectable(arg0: { providedIn: string; }): (target: typeof ImageService, context: ClassDecoratorContext<typeof ImageService>) => void | typeof ImageService {
+    throw new Error("Function not implemented.");
+}
+
